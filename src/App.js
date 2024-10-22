c77682515563d1ea065ea7698ab2765b10cdf8ed
kim <perfumeplaylist.gmail.com>
2024-10-22 18:52:53 +0900

    feat: 비동기 통신 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-export default class App {
-  constructor($target, params) {
-    console.log($target, params);
+import { $ } from "./utils.js";
+import weatherApi from "./service/WeatherApi.js";
+import Component from "./core/Components.js";
+import WeatherInfo from "./components/WeatherInfo.js";
+
+export default class App extends Component {
+  template() {
+    return `<main></main>`;
+  }
+
+  async mounted() {
+    const $main = $("main");
+    const weatherInfo = new WeatherInfo($main);
+    try {
+      const res = await weatherApi.getCurrentWeather({ q: "seoul" });
+      weatherInfo.setState({ ...res });
+    } catch (error) {
+      // new ErrorBoundary(error);
+    }
   }
 }
--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,5 +1,39 @@
-export default class WeatherInfo {
-  constructor($target, params) {
-    console.log($target, params);
+import Component from "../core/Components.js";
+import { $ } from "../utils.js";
+import Heading from "./common/Heading.js";
+
+export default class WeatherInfo extends Component {
+  template() {
+    return `<article>
+    <h1></h1>
+    ${this.state !== null && `<p class=time></p>`}
+    <section>
+      <div>
+        <img/>
+        <p class=weather_state></p>
+      </div>
+      <div>
+        <div>
+        <p class=temp_title></p>
+        <p class=current_temp></p>
+        </div>
+        <div>
+          <p class=temp_title></p>
+          <p class=temp_min_max></p>
+        </div>
+      </div>
+    </section>
+    </article>`;
+  }
+
+  mounted() {
+    const $title = $("h1");
+    const $time = $(".time");
+    const $icon = $("img");
+    const $state = $(".weather_state");
+    const $temp_title = $(".temp_title");
+    if (this.state !== null) {
+      new Heading($title, { level: 2, content: this.state.name });
+    }
   }
 }
